f54ed89aa60b0c85db20b6220f01b9fd644704f1
HiagoP <hiagoperonip@gmail.com>
2022-10-24 17:09:33 -0300

    Requisitos 1 ao 4 concluídos :rocket:

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { render, screen, userEvent } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import Provider from '../context/Provider';
+import renderWithRouter from './helpers/helpers';
 
 describe('Testes para a tela de Login', () => {
   // it('Testa se a página login é renderizada na rota "/"', () => {
   //   render(<App />);
   // });
   it('Testa se os campos de login estão na tela', () => {
-    render(<Provider><App /></Provider>);
+    renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
     const emailInput = screen.getByTestId('email-input');
     const passInput = screen.getByTestId('password-input');
 
     expect(emailInput).toBeInTheDocument();
     expect(passInput).toBeInTheDocument();
   });
-  it('Testa se o botão de login está desavilitado caso os campos sejam inválidos', () => {
-    render(<Provider><App /></Provider>);
+  it('Testa se o botão de login está desabilitado caso os campos sejam inválidos', () => {
+    renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
     const emailInput = screen.getByTestId('email-input');
     const passInput = screen.getByTestId('password-input');
     const loginBtn = screen.getByRole('button', {
@@ -26,5 +36,22 @@ describe('Testes para a tela de Login', () => {
     userEvent.type(emailInput, 'testeteste.com');
     userEvent.type(passInput, '123456');
     expect(loginBtn).toBeDisabled();
+  });
+
+  it('Testa se o botão de login está habilitado caso os campos sejam válidos', () => {
+    renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    const emailInput = screen.getByTestId('email-input');
+    const passInput = screen.getByTestId('password-input');
+    const loginBtn = screen.getByRole('button', {
+      name: /enter/i,
+    });
+
+    userEvent.type(emailInput, 'teste@teste.com');
+    userEvent.type(passInput, '1234567');
+    expect(loginBtn).not.toBeDisabled();
   });
 });
