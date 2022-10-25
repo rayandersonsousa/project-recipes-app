2aa34763e360fbac3a7918d7d529433960d2ed1f
RoberthDS <roberthgt158@gmail.com>
2022-10-25 17:11:36 -0300

    [requisito 58 e 59] e-mail armazenado em localStorage está visível

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,12 +3,13 @@ import AppContext from '../context/AppContext';
 import Header from './Header';
 
 function Profile() {
-  const { email } = useContext(AppContext);
+  const emailStorage = JSON.parse(localStorage.getItem('user'));
+  console.log('storage', emailStorage.email);
   return (
     <div>
       <Header titulo="Profile" pesquisa="false" />
       <h1>Profile</h1>
-      <h2 data-testid="profile-email">{email}</h2>
+      <h2 data-testid="profile-email">{emailStorage.email}</h2>
       <button type="button" data-testid="profile-done-btn">Done Recipes</button>
       <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
       <button type="button" data-testid="profile-logout-btn">Logout</button>
