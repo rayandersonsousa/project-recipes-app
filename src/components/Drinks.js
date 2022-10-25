c6c083607b73d09995ea8b6457cbf795d4477c22
Rayanderson Sousa <rayandersoncooper@gmail.com>
2022-10-25 16:14:21 -0300

    Requisitos 16 a 18 finalizados

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 function Drinks() {
   return (
     <div>
       <Header titulo="Drinks" pesquisa="true" />
       <h1>Drinks</h1>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 function Profile() {
   return (
     <div>
       <Header titulo="Profile" pesquisa="false" />
       <h1>Profile</h1>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
