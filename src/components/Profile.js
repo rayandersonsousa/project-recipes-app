f356f23ea4964c5e05cc3902b4803d782b2f984d
RoberthDS <roberthgt158@gmail.com>
2022-10-25 17:47:40 -0300

    [requisito 61]  redireciona para a rota correta

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,43 @@
-import React, { useContext } from 'react';
-import AppContext from '../context/AppContext';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from './Header';
 
 function Profile() {
-  const emailStorage = JSON.parse(localStorage.getItem('user'));
-  console.log('storage', emailStorage.email);
+  const storage = JSON.parse(localStorage.getItem('user'));
+  const emailStorage = storage.email;
+  const history = useHistory();
+
+  const handleClickDone = () => {
+    history.push('/done-recipes');
+  };
+
+  const handleClickFavorite = () => {
+    history.push('/favorite-recipes');
+  };
+
   return (
     <div>
       <Header titulo="Profile" pesquisa="false" />
       <h1>Profile</h1>
-      <h2 data-testid="profile-email">{emailStorage.email}</h2>
-      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+
+      <h2 data-testid="profile-email">{emailStorage}</h2>
+
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ handleClickDone }
+      >
+        Done Recipes
+
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ handleClickFavorite }
+      >
+        Favorite Recipes
+
+      </button>
       <button type="button" data-testid="profile-logout-btn">Logout</button>
     </div>
   );
