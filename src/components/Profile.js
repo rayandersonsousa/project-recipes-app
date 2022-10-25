13462fd0650622898ac9ccb0b42eeb53f2228053
RoberthDS <roberthgt158@gmail.com>
2022-10-25 16:28:32 -0300

    [requisito 57] todos os testId no lugar

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import Header from './Header';
 
 function Profile() {
+  const { email } = useContext(AppContext);
   return (
     <div>
       <Header titulo="Profile" pesquisa="false" />
       <h1>Profile</h1>
+      <h2 data-testid="profile-email">{email}</h2>
+      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
     </div>
   );
 }
