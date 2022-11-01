0166a93957af835d03bf71e3b5fdd0d74cf622ca
HiagoP <hiagoperonip@gmail.com>
2022-11-01 15:38:56 -0300

    Requisito 42 pronto

--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import blackHeart from '../images/blackHeartIcon.svg';
+import whiteHeart from '../images/whiteHeartIcon.svg';
 
 function RecipeInProgress(props) {
   const { match: { url } } = props;
@@ -14,14 +16,20 @@ function RecipeInProgress(props) {
   const [paginaAtual, setPaginaAtual] = useState('');
   const [fotoPagina, setFotoPagina] = useState('');
   const [nomeReceita, setNomeReceita] = useState('');
+  const [foiFavorito, setfoiFavorito] = useState(whiteHeart);
   // const [ingredientesFeitos, setIngredientesFeitos] = useState([]);
+  const [copiado, setCopiado] = useState(false);
+  const [marcados, setMarcados] = useState(0);
+  const [disabled, setDisabled] = useState(true);
   const arrayDeEscolhidos = [];
 
   useEffect(() => {
     async function buscarDados(pagina, id) {
       if (!localStorage.getItem('inProgressRecipes')) {
         localStorage.setItem('inProgressRecipes', []);
-        console.log('foi');
+      }
+      if (!localStorage.getItem('favoriteRecipes')) {
+        localStorage.setItem('favoriteRecipes', []);
       }
       if (pagina === 'meals') {
         const endpoint = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
@@ -59,19 +67,84 @@ function RecipeInProgress(props) {
     setIngredientes(arrayIngredientesFiltrados);
   }, [receita]);
 
+  useEffect(() => {
+    const favoritados = localStorage.getItem('favoriteRecipes');
+    const nameRecipe = receita[nomeReceita];
+    if (favoritados.includes(nameRecipe)) {
+      return setfoiFavorito(blackHeart);
+    }
+    return setfoiFavorito(whiteHeart);
+  }, [nomeReceita, foiFavorito]);
+
   const riscarSelecionado = ({ target }) => {
     const label = target.parentNode;
-    console.log(target.checked);
     const jaSalvos = localStorage.getItem('inProgressRecipes');
     arrayDeEscolhidos.push(target.name);
     localStorage.setItem('inProgressRecipes', [jaSalvos, arrayDeEscolhidos]);
+    const todosOsFavoritados = localStorage.getItem('inProgressRecipes');
+    const agoraVaiReceitasFavoritadas = todosOsFavoritados.split(',');
+    const arrayDefinitivo = agoraVaiReceitasFavoritadas
+      .filter((cadaReceita) => cadaReceita.length > 1);
+    localStorage.setItem('inProgressRecipes', arrayDefinitivo);
     if (label.classList.contains('riscarPalavra')) {
       label.classList.remove('riscarPalavra');
       target.checked = false;
     } else {
       label.classList.add('riscarPalavra');
       target.checked = true;
+      setMarcados((oldState) => oldState + 1);
     }
+  };
+
+  useEffect(() => {
+    const jaSalvos = localStorage.getItem('inProgressRecipes');
+    const novoArray = jaSalvos.split(',');
+    novoArray.forEach((cadaUm) => {
+      if (ingredientes.includes(cadaUm)) {
+        console.log(cadaUm);
+        return setMarcados((oldState) => oldState + 1);
+      }
+      if (marcados !== 0 && marcados === ingredientes.length) {
+        setDisabled(false);
+      }
+    });
+  }, [ingredientes]);
+
+  useEffect(() => {
+    if (marcados !== 0 && marcados === ingredientes.length) {
+      setDisabled(false);
+    }
+  }, [marcados]);
+
+  const favoritarReceita = (nome) => {
+    const pegarReceitasLocalStorage = localStorage.getItem('favoriteRecipes');
+    const novasReceitasFavoritadas = pegarReceitasLocalStorage.split(',');
+    const listaReceitasFavoritas = novasReceitasFavoritadas
+      .filter((cadaReceita) => cadaReceita.length > 1);
+    if (listaReceitasFavoritas.includes(nome)) {
+      const removerNome = listaReceitasFavoritas
+        .filter((cadaFav) => cadaFav !== nome);
+      localStorage.setItem('favoriteRecipes', removerNome);
+      const todosOsFavoritados = localStorage.getItem('favoriteRecipes');
+      const agoraVaiReceitasFavoritadas = todosOsFavoritados.split(',');
+      const arrayDefinitivo = agoraVaiReceitasFavoritadas
+        .filter((cadaReceita) => cadaReceita.length > 1);
+      localStorage.setItem('favoriteRecipes', arrayDefinitivo);
+      setfoiFavorito(whiteHeart);
+    } else {
+      localStorage.setItem('favoriteRecipes', [listaReceitasFavoritas, nome]);
+      const todosOsFavoritados = localStorage.getItem('favoriteRecipes');
+      const agoraVaiReceitasFavoritadas = todosOsFavoritados.split(',');
+      const arrayDefinitivo = agoraVaiReceitasFavoritadas
+        .filter((cadaReceita) => cadaReceita.length > 1);
+      localStorage.setItem('favoriteRecipes', arrayDefinitivo);
+      setfoiFavorito(blackHeart);
+    }
+  };
+
+  const copiarProClipBoard = () => {
+    navigator.clipboard.writeText(`http://localhost:3000/${paginaLocal}/${idPag}`);
+    return setCopiado(true);
   };
 
   return (
@@ -121,14 +194,37 @@ function RecipeInProgress(props) {
                           />
                         )
                     }
-
                   </label>
                 ),
               )
             }
-            <button type="button" data-testid="share-btn">Share</button>
-            <button type="button" data-testid="favorite-btn">Favorite</button>
-            <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
+            <button
+              type="button"
+              onClick={ copiarProClipBoard }
+              data-testid="share-btn"
+            >
+              Share
+            </button>
+            {
+              copiado && (<p>Link copied!</p>)
+            }
+            <button
+              type="button"
+              onClick={ () => favoritarReceita(receita[nomeReceita]) }
+            >
+              <img
+                data-testid="favorite-btn"
+                src={ foiFavorito }
+                alt="favoritado"
+              />
+            </button>
+            <button
+              type="button"
+              data-testid="finish-recipe-btn"
+              disabled={ disabled }
+            >
+              Finish Recipe
+            </button>
           </div>
         )
       }
