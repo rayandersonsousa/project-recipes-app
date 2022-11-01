7f904feb0b1b017391bbf84d8592f2d18c734f20
RoberthDS <roberthgt158@gmail.com>
2022-11-01 19:40:30 -0300

    testes para recipes

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { act, screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { within } from '@testing-library/dom';
 import App from '../App';
 import Provider from '../context/Provider';
 import renderWithRouter from './helpers/helpers';
@@ -11,7 +10,7 @@ describe('Testes para Recipes', () => {
   const senhaString = 'password-input';
   const emailDoUsuario = 'teste@teste.com';
   it('test Radio', async () => {
-    const { history } = renderWithRouter(
+    renderWithRouter(
       <Provider>
         <App />
       </Provider>,
@@ -24,12 +23,9 @@ describe('Testes para Recipes', () => {
     userEvent.type(emailInput, emailDoUsuario);
     userEvent.type(passInput, '1234567');
     userEvent.click(loginBtn);
-
-
-    const text = screen.getByRole('heading', {  name: /meals/i})
-    expect(text).toBeInTheDocument()
+    const text = screen.getByRole('heading', { name: /meals/i });
+    expect(text).toBeInTheDocument();
     const radioAll = screen.getByTestId('All-category-filter');
     expect(radioAll).toBeInTheDocument();
-    
   });
 });
